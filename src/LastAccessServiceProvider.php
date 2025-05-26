bf2493c14cc4ce8132a6aaa548a3b36708c22e77
Yassmin <yasminmostafasmsm2010@gmail.com>
2025-05-26 12:46:13 +0300

    fix issues

--- src/LastAccessServiceProvider.php
+++ src/LastAccessServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Ouredu\UserLastAccess;
 
+use Illuminate\Foundation\Http\Events\RequestHandled;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Ouredu\UserLastAccess\Listeners\UserLastAccessListener;
 
@@ -9,20 +11,21 @@ class LastAccessServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        // Update paths to point to the correct location of the migrations folder
+        // Publish and load migrations
         $this->publishes([
-            __DIR__.'/../database/migrations/' => database_path('migrations'),
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
         ], 'migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-        $this->app->terminating(function () {
-            (new UserLastAccessListener())->handle();
+        // Listen for the end of every request
+        Event::listen(RequestHandled::class, function (RequestHandled $event) {
+            \Log::error('RequestHandled event FIRED');
+            \Log::error('Request path: ' . $event->request->path());
+            if ($event->request->is('api/*')) {
+                \Log::error('Calling UserLastAccessListener from ServiceProvider');
+                (new UserLastAccessListener())->handle($event->request);
+            }
         });
-    }
 
-    public function register()
-    {
-        //
     }
 }
--- src/Listeners/UserLastAccessListener.php
+++ src/Listeners/UserLastAccessListener.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Log;
 use Ouredu\UserLastAccess\Models\UserLastAccess;
 use Illuminate\Support\Facades\Auth;
 
+
 class UserLastAccessListener
 {
     public function handle($event = null): void
